48095bf72da29bd547f74d4f0023b6cf41183550
albertsalendah@gmail.com <ertsalendah@gmail.com>
2023-06-10 17:22:34 +0700

    testNO

--- src/testNO.ts
+++ src/testNO.ts
@@ -8,54 +8,55 @@ const datas = [{
     "Prodi_Registrasi_Ulang": "702023003"
 },{
     "No_Pendaftaran":"02",
-    "Nama_Pendaftar": "Richard Salendah 02",
-    "No_HP": "082138345212",
+    "Nama_Pendaftar": "Ocha",
+    "No_HP": "08112822278",
     "Tahun_Akademik":"2023-2024",
     "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
     "Prodi_Registrasi_Ulang": "702023003"
 },{
     "No_Pendaftaran":"03",
-    "Nama_Pendaftar": "Richard Salendah 03",
-    "No_HP": "082138345212",
+    "Nama_Pendaftar": "Nina",
+    "No_HP": "085640551818",
     "Tahun_Akademik":"2023-2024",
     "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
     "Prodi_Registrasi_Ulang": "702023003"
 },{
     "No_Pendaftaran":"04",
-    "Nama_Pendaftar": "Richard Salendah 04",
-    "No_HP": "082138345212",
-    "Tahun_Akademik":"2023-2024",
-    "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
-    "Prodi_Registrasi_Ulang": "702023003"
-},{
-    "No_Pendaftaran":"05",
-    "Nama_Pendaftar": "Richard Salendah 05",
-    "No_HP": "082138345212",
-    "Tahun_Akademik":"2023-2024",
-    "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
-    "Prodi_Registrasi_Ulang": "702023003"
-},{
-    "No_Pendaftaran":"06",
-    "Nama_Pendaftar": "Richard Salendah 06",
-    "No_HP": "082138345212",
-    "Tahun_Akademik":"2023-2024",
-    "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
-    "Prodi_Registrasi_Ulang": "702023003"
-},{
-    "No_Pendaftaran":"07",
-    "Nama_Pendaftar": "Richard Salendah 07",
-    "No_HP": "082138345212",
-    "Tahun_Akademik":"2023-2024",
-    "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
-    "Prodi_Registrasi_Ulang": "702023003"
-},{
-    "No_Pendaftaran":"08",
-    "Nama_Pendaftar": "Richard Salendah 08",
-    "No_HP": "082138345212",
+    "Nama_Pendaftar": "Gilang",
+    "No_HP": "087719337290",
     "Tahun_Akademik":"2023-2024",
     "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
     "Prodi_Registrasi_Ulang": "702023003"
 },
+// {
+//     "No_Pendaftaran":"05",
+//     "Nama_Pendaftar": "Richard Salendah 05",
+//     "No_HP": "082138345212",
+//     "Tahun_Akademik":"2023-2024",
+//     "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
+//     "Prodi_Registrasi_Ulang": "702023003"
+// },{
+//     "No_Pendaftaran":"06",
+//     "Nama_Pendaftar": "Richard Salendah 06",
+//     "No_HP": "082138345212",
+//     "Tahun_Akademik":"2023-2024",
+//     "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
+//     "Prodi_Registrasi_Ulang": "702023003"
+// },{
+//     "No_Pendaftaran":"07",
+//     "Nama_Pendaftar": "Richard Salendah 07",
+//     "No_HP": "082138345212",
+//     "Tahun_Akademik":"2023-2024",
+//     "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
+//     "Prodi_Registrasi_Ulang": "702023003"
+// },{
+//     "No_Pendaftaran":"08",
+//     "Nama_Pendaftar": "Richard Salendah 08",
+//     "No_HP": "082138345212",
+//     "Tahun_Akademik":"2023-2024",
+//     "Status_Registrasi_Ulang": "Diterima - PEMAMIK 7 2023",
+//     "Prodi_Registrasi_Ulang": "702023003"
+// },
 ];
 
 export const applicants = datas.map((data) => ({
