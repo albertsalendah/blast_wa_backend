80382fcd327a89af89503f68ecca4304ba658b04
albertsalendah@gmail.com <ertsalendah@gmail.com>
2023-11-24 15:39:37 +0700

    .

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -2,7 +2,7 @@ export const phoneNumberFormatter = function (number: String) {
 	let formatted = number.replace(/\D/g, '');
 
 	if (formatted.startsWith('0')) {
-		formatted = '62' + formatted.substr(1);
+		formatted = '670' + formatted.substr(1);
 	}
 	//@c.us
 	if (!formatted.endsWith('@s.whatsapp.net')) {
